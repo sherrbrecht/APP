158fd7357067751cf25e1aeae1b2526a798c2c15
Stephane Herrbrecht <stephane.herrbrecht@gmail.com>
2018-01-22 15:18:31 +0100

    Initialisation Solution

---
+++ AAP/MembersConso/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MembersConso
+{
+    [Activity(Label = "MembersConso", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
